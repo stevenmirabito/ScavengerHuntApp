c131857dc9cee445de693d6cb109851fb5bb9e67
Steven Mirabito <steven@stevenmirabito.com>
2016-01-26 18:45:36 -0500

    Initial Commit

---
+++ Scavenger Hunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Steven Mirabito on 1/26/16.
+//  Copyright © 2016 StevTek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
