6416938239c0dcc3a08a68ca94fa909cdf3b9fc2
Steven Mirabito <steven@stevenmirabito.com>
2016-01-26 21:18:51 -0500

    Added navigation buttons and initial action connections

--- Scavenger Hunt/AddViewController.swift
+++ Scavenger Hunt/AddViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
