79fd697dfedd2d465a882d87a1eefd6f85de2d03
Steven Mirabito <steven@stevenmirabito.com>
2016-01-27 20:18:40 -0500

    Added .gitignore

--- Scavenger Hunt/AddItemViewController.swift
+++ Scavenger Hunt/AddItemViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  AddTableViewController.swift
 //  Scavenger Hunt
 //
-//  Created by Steven Mirabito on 1/26/16.
+//  Created by Steven Mirabito on 1/27/16.
 //  Copyright © 2016 StevTek. All rights reserved.
 //
 
 import UIKit
 
-class AddViewController: UIViewController {
-    @IBOutlet weak var textField: UITextField!
-    
+class AddItemViewController: UITableViewController {
     var newItem: ScavengerHuntItem?
-
-    @IBAction func cancel(sender: AnyObject) {
+    
+    func cancel(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
         if identifier == "DoneItem" {
-            if let name = textField.text {
+            //if let name = textField.text {
+            let name = "temp"
+            if true {
                 if name.isEmpty {
                     let emptyAlertController = UIAlertController(title: "Invalid Item Name", message: "The item name cannot be blank", preferredStyle: .Alert)
                     let emptyOkAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
@@ -33,13 +33,15 @@ class AddViewController: UIViewController {
                 }
             }
         }
-
+        
         return true
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "DoneItem" {
-            if let name = textField.text {
+            //if let name = textField.text {
+            let name = "temp"
+            if true {
                 if !name.isEmpty {
                     newItem = ScavengerHuntItem(name: name)
                 }
@@ -49,14 +51,14 @@ class AddViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        textField.becomeFirstResponder()
+        //textField.becomeFirstResponder()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
